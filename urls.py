7ba2f45fcb097b2de681fac29001121791956f4b
Mr_Programmer <155601997+Umaralthaf@users.noreply.github.com>
2024-06-05 19:41:53 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from django.urls import path
 #from django.conf.urls.static import static
 #from django.conf import  settings
-from logo.views import login_view
+from logo.views import login_view,login_successful
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',login_view,name="login_view"),
-
+    path('hi',login_successful,name="login_successful"),
 ]
